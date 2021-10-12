c2decf3db2e312e02e5ce897c8434a66ac859ad2
Marco Starker <marco.starker@gmx.net>
2021-10-12 10:42:08 +0200

    removing legacy support

--- rollup.config.js
+++ rollup.config.js
@@ -3,31 +3,6 @@ import babel from 'rollup-plugin-babel';
 import {terser} from "rollup-plugin-terser";
 import clear from "rollup-plugin-clear";
 
-const babelOptionsES5 = {
-    runtimeHelpers:  false,
-    externalHelpers: false,
-    babelrc:         false,
-    presets:         [
-        [
-            "@babel/env",
-            {
-                targets: {
-                    ie:      '11',
-                    edge:    '17',
-                    firefox: '60',
-                    chrome:  '71',
-                    safari:  '11.1',
-                },
-            }
-        ]
-    ],
-    plugins:         [
-        ["@babel/plugin-proposal-class-properties", {loose: true}],
-        ["@babel/plugin-proposal-private-property-in-object", {loose: true}],
-        ["@babel/plugin-proposal-private-methods", { loose: true }],
-    ]
-};
-
 const babelOptionsES2018 = {
     runtimeHelpers:  false,
     externalHelpers: false,
@@ -41,48 +16,17 @@ const babelOptionsES2018 = {
 
 // https://rollupjs.org/guide/en#big-list-of-options
 export default [
-    // ES2015 Minified
-    {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/translator.min.js',
-            format:    'umd',
-            name:      'Translator',
-            compact:   true,
-            sourcemap: true,
-        },
-        plugins: [
-            resolve(),
-            clear({targets: ['./dist']}),
-            babel(babelOptionsES5),
-            terser(),
-        ]
-    },
-    // ES2015 None-Minified
-    {
-        input:   './src/index.js',
-        output:  {
-            file:      './dist/translator.js',
-            format:    'umd',
-            name:      'Translator',
-            compact:   false,
-            sourcemap: true,
-        },
-        plugins: [
-            resolve(),
-            babel(babelOptionsES5),
-        ]
-    },
     // ES Modules Minified
     {
         input:   './src/index.js',
         output:  {
-            file:      './dist/translator.esm.min.js',
+            file:      './dist/translator.min.js',
             format:    'esm',
             compact:   true,
             sourcemap: true,
         },
         plugins: [
+            clear({targets: ['./dist']}),
             resolve(),
             babel(babelOptionsES2018),
             terser(),
@@ -92,7 +36,7 @@ export default [
     {
         input:   './src/index.js',
         output:  {
-            file:      './dist/translator.esm.js',
+            file:      './dist/translator.js',
             format:    'esm',
             compact:   false,
             sourcemap: true,
